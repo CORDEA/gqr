fd706e3c9273cc2f7248734fc8e27502b5862e47
Yoshihiro Tanaka <contact@cordea.jp>
2020-08-22 18:17:19 +0900

    define mode and version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use image::RgbaImage;
+
+const SIZE: u32 = 500;
+
+enum Version {
+    M1,
+}
+
+enum Mode {
+    NUMBER,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut img = RgbaImage::new(SIZE, SIZE);
+
+    img.save("gqr.png").unwrap();
 }
